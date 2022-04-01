2d440ced1681ce90cc3b0760db97d908d5f1d4a6
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-01 22:23:16 +0100

    Create layout.blade.php

---
+++ layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Classroom form</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body style="background-color:#E8E4C9;">
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
