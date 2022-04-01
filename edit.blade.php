a3728ef2542f1d7377d9b8bd9e047c72ef4d56cf
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-01 22:25:13 +0100

    Create edit.blade.php

---
+++ edit.blade.php
@@ -0,0 +1,79 @@
+@extends('rooms.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Classroom</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('rooms.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There are some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('rooms.update',$room->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>IdRoom</strong>
+                    <input type="text" name="idRoom" value="{{ $room->idRoom }}" class="form-control" placeholder="Enter the name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Type</strong>
+                    <input type="text" name="type" value="{{ $room->type }}" class="form-control"
+                           placeholder="Enter the type">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Floor</strong>
+                    <input type="text" name="floor" value="{{ $room->floor }}" class="form-control"
+                           placeholder="Enter the floor">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Capacity</strong>
+                    <input type="number" name="capacity" value="{{ $room->capacity }}" class="form-control"
+                           placeholder="Enter the capacity">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Particular</strong>
+                    <input type="text" name="particular" value="{{ $room->particular }}" class="form-control"
+                           placeholder="Is it particular?">
+                </div>
+            </div>
+
+
+
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+
+
+    </form>
+
+@endsection
