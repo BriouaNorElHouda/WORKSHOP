7c4988edfeab6b307ffa96dde2ed4dedc0daaa09
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-01 18:05:44 +0100

    Create migrationroom.php

---
+++ migrationroom.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTbl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->string('id');
+            $table->string('type');
+            $table->string('floor');
+            $table->integer('capacity');
+            $table->string('particular');
+
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
