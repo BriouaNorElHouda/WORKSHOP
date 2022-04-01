46ed4b508c72a8ec9a815c1df3709be8fb19d709
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-01 22:26:15 +0100

    Create show.blade.php

---
+++ show.blade.php
@@ -0,0 +1,49 @@
+@extends('rooms.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Classroom</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('rooms.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>IdRoom:</strong>
+                {{ $room->idRoom }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Type</strong>
+                {{ $room->type }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Floor</strong>
+                {{ $room->floor }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Capacity</strong>
+                {{ $room->capacity }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Particular</strong>
+                {{ $room->particular }}
+            </div>
+        </div>
+
+
+    </div>
+@endsection
