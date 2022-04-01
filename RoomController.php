ca62f3c0bef2ea4da22af391a371d94791465ccb
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-01 18:04:08 +0100

    Create RoomController.php

---
+++ RoomController.php
@@ -0,0 +1,114 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Room::latest()->paginate(5);
+
+        return view('rooms.index',compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('rooms.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id' => 'required|unique:rooms|regex:/[a-zA-Z0-9\s]+/',
+            'type' => 'required',
+            'floor' => 'required|numeric',
+            'capacity' => 'required',
+            'particular' => 'required',
+
+        ]);
+
+        Room::create($request->all());
+
+        return redirect()->route('rooms.index')
+            ->with('success','Room created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Room $room
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Room $room)
+    {
+        return view('rooms.show',compact('room'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Room $room)
+    {
+        return view('rooms.edit',compact('room'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Room $room)
+    {
+        $request->validate([
+            'id' => 'required|unique:rooms|regex:/[a-zA-Z0-9\s]+/',
+            'type' => 'required',
+            'floor' => 'required|numeric',
+            'capacity' => 'required',
+            'particular' => 'required',
+
+        ]);
+
+        $room->update($request->all());
+
+        return redirect()->route('rooms.index')
+            ->with('success','Room updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Room $room)
+    {
+        $room->delete();
+
+        return redirect()->route('rooms.index')
+            ->with('success','Room deleted successfully');
+    }
+}
