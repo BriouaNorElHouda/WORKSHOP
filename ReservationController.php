ded6a8655e10ef159cab0d9357dae09748a901a9
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-15 23:48:12 +0100

    Create ReservationController.php

---
+++ ReservationController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Reservation::latest()->paginate(5);
+
+        return view('reservations.index',compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('reservations.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'idReservation' => 'required|unique:reservations|numeric',
+            'idRoom' => 'required',
+            'idUser' => 'required|numeric',
+            'date' => 'required',
+            'state' => 'required',
+            'objective' => 'required',
+            ]);
+        Reservation::create($request->all());
+
+        return redirect()->route('reservations.index')
+            ->with('success','Reservation created successfully.');
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Reservation  $reservation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reservation $reservation)
+    {
+        return view('reservations.show',compact('reservation'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Reservation  $reservation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reservation $reservation)
+    {
+        return view('reservations.edit',compact('reservation'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Reservation  $reservation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reservation $reservation)
+    {
+        $request->validate([
+            'idReservation' => 'required|unique:reservations|numeric',
+            'idRoom' => 'required',
+            'idUser' => 'required|numeric',
+            'date' => 'required',
+            'state' => 'required',
+            'objective' => 'required',
+        ]);
+        Reservation::update($request->all());
+
+        return redirect()->route('reservations.index')
+            ->with('success','Reservation updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Reservation  $reservation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reservation $reservation)
+    {
+        $reservation->delete();
+
+        return redirect()->route('reservations.index')
+            ->with('success','Reservation deleted successfully');
+    }
+}
