59d2be61a443f0f04eab511658cbd26519638686
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-15 23:41:38 +0100

    Update terminalcode.php

--- terminalcode.php
+++ terminalcode.php
@@ -1,5 +1,7 @@
 php artisan make:migration create_room_tbl --create=rooms
+php artisan make:migration create_reservation_tbl --create=reservations
 
 
 
 php artisan make:controller RoomController --resource --model=Room
+php artisan make:controller ReservationController --resource --model=Reservation
