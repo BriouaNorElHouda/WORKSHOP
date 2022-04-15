15f7fad90fc4a79d9c84c69a9af236921dbbd2ba
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-15 23:39:48 +0100

    Update web.php

--- web.php
+++ web.php
@@ -1,4 +1,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UserController;
 
 Route::resource('rooms', RoomController::class);
+Route::resource('reservations', ReservationController::class);
+Route::resource('users', UserController::class);
