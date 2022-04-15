d83aebdfefff35e4568758e8308945f6e83c5905
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-15 18:03:43 +0100

    Update create.blade.php

--- create.blade.php
+++ create.blade.php
@@ -7,7 +7,7 @@
                 <h2>Add New Classroom</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('rooms.index') }}"> Back</a>
+                <a class="btn btn-primary" href="{{ route('rooms.index') }}"> Cancel</a>
             </div>
         </div>
     </div>
@@ -30,39 +30,46 @@
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>IdRoom:</strong>
-                    <input type="text" name="idRoom" class="form-control" placeholder="Enter the name">
+                    <input required= required type="text" name="idRoom" class="form-control"
+                           placeholder="Enter the name" value="{{old('idRoom')}}" >
+                    <li class="rq">*Enter numbers and alphabets only.</li>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Type:</strong>
-                    <select class="form-control" style="height:150px" name="type">
+                    <select  class="form-control" style="height:150px" name="type"  value="{{old('type')}}" >
                         <option disabled="disabled" selected="selected">--Choose a type--</option>
-                        <option value=”DWR”> Directed work Room </option>
-                        <option value=”PWR”> Practical Work Room </option>
-                        <option value=”AMPHI”>AMPHI</option>
+                        <option required="" value=”DWR” @if(old('type')== 'DWR') selected @endif> Directed work Room  </option>
+                        <option required="" value=”PWR” @if(old('type')== 'PWR') selected @endif> Practical Work Room </option>
+                        <option required="" value=”AMPHI” @if(old('type') == 'AMPHI') selected @endif>AMPHI </option>
+
                     </select>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Floor:</strong>
-                    <input type="text" name="floor" class="form-control" placeholder="Enter the floor">
+                    <input required= required type="text" name="floor" class="form-control"
+                           placeholder="Enter the floor" value="{{old('floor')}}">
+                    <li class="rq">*Enter numbers and alphabets only.</li>
                 </div>
             </div>
 
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Capacity:</strong>
-                    <input type="number" name="capacity" class="form-control" min="2" max="500">
+                    <input required= required type="number" name="capacity" class="form-control" min="2" max="500" value="{{old('capacity')}}">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Particular:</strong>
-                    <input type="radio"  name="particular" value="True" class="form-control">
+                    <input required= required  type="radio"  name="particular" value="True"class="form-control"
+                          @if(old('particular')=='True') checked @endif>
                     <label for="True">True</label><br>
-                     <input type="radio"  name="particular" value="False" class="form-control">
+                     <input required= required type="radio"  name="particular" value="False" class="form-control"
+                            value="False"@if(old('particular')=='False') checked @endif>
                     <label for="False">False</label><br>
 
                 </div>
