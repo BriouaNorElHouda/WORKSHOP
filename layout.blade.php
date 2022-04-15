ffca534127b2c40fe00d369de85c3e7f16a74479
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-04-15 18:06:50 +0100

    Update layout.blade.php

--- layout.blade.php
+++ layout.blade.php
@@ -4,7 +4,7 @@
     <title>Classroom form</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
-<body style="background-color:#B5D7B5">
+<body >
 
 <div class="container">
     @yield('content')
